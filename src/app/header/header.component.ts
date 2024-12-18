7f99f0200e12e9f79f7ddc36c5185ad9c037ca9a
mechbrian <mechbrian062@gmail.com>
2024-12-18 16:19:28 -0500

    modify footer and the button in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  constructor(private viewpostScroller: ViewportScroller) {}
+  scrollToFooter(): void {
+    this.viewpostScroller.scrollToAnchor('footer');
+  }
 }
